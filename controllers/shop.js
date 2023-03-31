7564f96f41494d41a13355ed2dcb9c1dc32babd3
cengamat <azurnaci@cultwinesltd.com>
2023-03-31 17:14:55 +0100

    Fetching a Single Product with the "where" Condition

--- controllers/shop.js
+++ controllers/shop.js
@@ -15,13 +15,16 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const productId = req.params.productId;
-  Product.findById(productId, (product) => {
-    res.render("shop/product-detail", {
-      product: product,
-      pageTitle: product.pageTitle,
-      path: "/products",
-    });
-  });
+  Product.findById(productId)
+    .then(([product]) => {
+      console.log(product);
+      res.render("shop/product-detail", {
+        product: product[0],
+        pageTitle: product[0].title,
+        path: "/products",
+      });
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.getIndex = (req, res, next) => {
