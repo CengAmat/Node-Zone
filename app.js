5d43de6ca53f45697340d04232616b3cf1c5b791
cengamat <azurnaci@cultwinesltd.com>
2023-04-04 15:43:34 +0100

    Syncing JS Definitions to the Database

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
-
+const sequelize = require("./util/database");
+const product = require("./models/product");
 const app = express();
 
 app.set("view engine", "ejs");
@@ -21,4 +22,12 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3001);
+sequelize
+  .sync()
+  .then((result) => {
+    // console.log(result);
+    app.listen(3001);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- util/database.js
+++ util/database.js
@@ -1,15 +1,4 @@
-// const mysql = require("mysql2");
-
-// const pool = mysql.createPool({
-//   host: "localhost",
-//   user: "root",
-//   database: "node-complete",
-//   password: "2232824aZ",
-// });
-
-// module.exports = pool.promise();
-
-const Sequelize = require("sequelize");
+const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize("node-complete", "root", "2232824aZ", {
   dialect: "mysql",
