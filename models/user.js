277ee0adbe320f77892e96e7427ca7b2bbdc62e3
cengamat <azurnaci@cultwinesltd.com>
2023-04-13 13:51:20 +0100

    Adding Relational Order Data

--- models/user.js
+++ models/user.js
@@ -91,9 +91,18 @@ class User {
 
   addOrder() {
     const db = getDb();
-    return db
-      .collection("orders")
-      .insertOne(this.cart)
+    return this.getCart()
+      .then((products) => {
+        const order = {
+          items: products,
+          user: {
+            _id: new ObjectId(this._id),
+            name: this.name,
+            email: this.email,
+          },
+        };
+        return db.collection("orders").insertOne(order);
+      })
       .then((result) => {
         this.cart = { items: [] };
         return db
@@ -103,6 +112,11 @@ class User {
             { $set: { cart: { items: [] } } }
           );
       });
+  }
+
+  getOrders() {
+    const db = getDb();
+    // return db.collection('orders').find()
   }
 
   static findById(userId) {
