12409e055752a6b661a8e1cd626a732736a83c94
cengamat <azurnaci@cultwinesltd.com>
2023-04-13 10:30:10 +0100

    Working on Cart Items & Orders

--- models/user.js
+++ models/user.js
@@ -1,12 +1,15 @@
 const getDb = require("../util/database").getDb;
 const mongodb = require("mongodb");
+const { get } = require("../routes/admin");
 
 const ObjectId = mongodb.ObjectId;
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, id) {
     this.name = username;
     this.email = email;
+    this.cart = cart;
+    this._id = id;
   }
 
   save() {
@@ -20,6 +23,20 @@ class User {
       .catch((err) => {
         console.log(err);
       });
+  }
+
+  addToCart(product) {
+    // const cartProduct = this.cart.items.findIndex((cp) => {
+    //   return cp._id === product._id;
+    // });
+    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    const db = getDb();
+    return db
+      .collection("users")
+      .updateOne(
+        { _id: new ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      );
   }
 
   static findById(userId) {
