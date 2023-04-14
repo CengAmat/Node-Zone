68e9f33f6cb5938228e76348a0a3b0db3b4396cd
cengamat <azurnaci@cultwinesltd.com>
2023-04-14 15:22:20 +0100

    Saving Data Through Mongoose

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 const errorController = require("./controllers/error");
-const User = require("./models/user");
+// const User = require("./models/user");
 
 const app = express();
 
@@ -18,14 +18,14 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use((req, res, next) => {
-  User.findById("6436e462290813f2a06b1de9")
-    .then((user) => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch((err) => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById("6436e462290813f2a06b1de9")
+//     .then((user) => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch((err) => console.log(err));
+// });
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -34,7 +34,7 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-    "mongodb+srv://ametzurnaci:J2Dy3jl6yGxy1P6o@cluster0.iipdgc3.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb+srv://ametzurnaci:J2Dy3jl6yGxy1P6o@cluster0.iipdgc3.mongodb.net/shop?retryWrites=true&w=majority"
   )
   .then((result) => {
     app.listen(3001);
