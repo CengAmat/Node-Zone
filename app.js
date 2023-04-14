69763b488cad8fb8aa2085e2f9a65b556470b5c2
cengamat <azurnaci@cultwinesltd.com>
2023-04-14 12:41:02 +0100

    Connecting to the MongoDB Server with Mongoose

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const errorController = require("./controllers/error");
-const mongoConnect = require("./util/database").mongoConnect;
 const User = require("./models/user");
 
 const app = express();
@@ -32,6 +32,13 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3001);
-});
+mongoose
+  .connect(
+    "mongodb+srv://ametzurnaci:J2Dy3jl6yGxy1P6o@cluster0.iipdgc3.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then((result) => {
+    app.listen(3001);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
