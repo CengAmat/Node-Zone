3a6a623f033f9bf79648777a9a0e990660be620c
cengamat <azurnaci@cultwinesltd.com>
2023-04-20 18:13:56 +0100

    Updating Products

--- controllers/admin.js
+++ controllers/admin.js
@@ -55,9 +55,14 @@ exports.postEditProduct = (req, res, next) => {
   const productId = req.body.productId;
   const { title, price, imageUrl, description } = req.body;
 
-  const product = new Product(title, price, description, imageUrl, productId);
-  product
-    .save()
+  Product.findById(productId)
+    .then((product) => {
+      product.title = title;
+      product.price = price;
+      product.imageUrl = imageUrl;
+      product.description = description;
+      return product.save();
+    })
     .then((result) => {
       console.log("UPDATED PRODUCT!");
       res.redirect("/admin/products");
@@ -76,7 +81,7 @@ exports.postDeleteProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then((products) => {
       res.render("admin/products", {
         prods: products,
