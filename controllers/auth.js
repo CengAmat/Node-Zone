29491482271e27accd730481aec3da93bf4a8d6f
cengamat <azurnaci@cultwinesltd.com>
2023-05-04 22:55:13 +0100

    Configuring Cookies

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,13 @@
 exports.getLogin = (req, res, next) => {
-  const isLoggedIn = req.get("Cookie").trim().split("=")[1];
+  //   const isLoggedIn = req.get("Cookie").trim().split("=")[1];
   res.render("auth/login", {
     pageTitle: "Login",
     path: "/login",
-    isAuthenticated: isLoggedIn,
+    isAuthenticated: false,
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader("Set-Cookie", "loggedIn=true");
+  res.setHeader("Set-Cookie", "loggedIn=true; HttpOnly");
   res.redirect("/");
 };
