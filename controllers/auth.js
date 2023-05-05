b4f1585fb665813c1b1fb61236ee261cb0f32fad
cengamat <azurnaci@cultwinesltd.com>
2023-05-05 11:17:22 +0100

    Using the Session Middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 exports.getLogin = (req, res, next) => {
   //   const isLoggedIn = req.get("Cookie").trim().split("=")[1];
+  console.log(req.session);
   res.render("auth/login", {
     pageTitle: "Login",
     path: "/login",
@@ -8,6 +9,6 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader("Set-Cookie", "loggedIn=true; HttpOnly");
+  req.session.isLoggedIn = true;
   res.redirect("/");
 };
