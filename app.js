04a5dd14e0e8dc52f9e114716122133ceec667aa
cengamat <azurnaci@cultwinesltd.com>
2023-05-06 21:05:28 +0100

    Implementing an Authentication Flow

--- app.js
+++ app.js
@@ -56,19 +56,6 @@ app.use(errorController.get404);
 mongoose
   .connect(MONGODB_URI)
   .then((result) => {
-    User.findOne().then((user) => {
-      if (!user) {
-        const user = new User({
-          name: "Amat",
-          email: "cengamat@gmail.com",
-          cart: {
-            items: [],
-          },
-        });
-        user.save();
-      }
-    });
-
     app.listen(3001);
   })
   .catch((err) => {
