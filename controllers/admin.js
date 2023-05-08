05013131eb5c0df03fcf1e1f314ba2d797322cfc
cengamat <azurnaci@cultwinesltd.com>
2023-05-08 18:58:17 +0100

    Adding Authorization

--- controllers/admin.js
+++ controllers/admin.js
@@ -85,7 +85,7 @@ exports.postDeleteProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.find()
+  Product.find({ userId: req.user._id })
     // .select("title price -_id")
     // .populate("userId")
     .then((products) => {
