b7c15d82532bb44e8590c02740bd10ebe45eb139
cengamat <azurnaci@cultwinesltd.com>
2023-05-08 16:48:27 +0100

    Resetting Passwords

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,17 @@
 const bcrypt = require("bcryptjs");
+const nodemailer = require("nodemailer");
+const sendgridTransport = require("nodemailer-sendgrid-transport");
 
 const User = require("../models/user");
+
+const transporter = nodemailer.createTransport(
+  sendgridTransport({
+    auth: {
+      api_key:
+        "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",
+    },
+  })
+);
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash("error");
@@ -90,6 +101,14 @@ exports.postSignup = (req, res, next) => {
         })
         .then(() => {
           res.redirect("/login");
+          return transporter
+            .sendMail({
+              to: email,
+              from: "cengamat@gmail.com",
+              subject: "Signup succeeded!",
+              html: "<h1>You successfully signed up!</h1>",
+            })
+            .catch((err) => console.log(err));
         });
     })
     .catch((err) => console.log(err));
@@ -99,5 +118,19 @@ exports.postLogout = (req, res, next) => {
   req.session.destroy((err) => {
     console.log(err);
     res.redirect("/");
+  });
+};
+
+exports.getReset = (req, res, next) => {
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+  res.render("auth/reset", {
+    path: "/reset",
+    pageTitle: "Reset Password",
+    errorMessage: message,
   });
 };
