c5403c347e3b613dd28dd56f6bb223af33f944b7
cengamat <azurnaci@cultwinesltd.com>
2023-05-16 17:05:34 +0100

    Keeping User Input

--- controllers/auth.js
+++ controllers/auth.js
@@ -25,6 +25,10 @@ exports.getLogin = (req, res, next) => {
     path: "/login",
     pageTitle: "Login",
     errorMessage: message,
+    oldInput: {
+      email: "",
+      password: "",
+    },
   });
 };
 
@@ -39,6 +43,11 @@ exports.getSignup = (req, res, next) => {
     path: "/signup",
     pageTitle: "Signup",
     errorMessage: message,
+    oldInput: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
   });
 };
 
@@ -51,6 +60,7 @@ exports.postLogin = (req, res, next) => {
       path: "/login",
       pageTitle: "login",
       errorMessage: errors.array()[0].msg,
+      oldInput: { email: email, password: password },
     });
   }
   User.findOne({ email: email })
@@ -81,7 +91,7 @@ exports.postLogin = (req, res, next) => {
 };
 
 exports.postSignup = (req, res, next) => {
-  const { email, password } = req.body;
+  const { email, password, confirmPassword } = req.body;
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     console.log(errors.array());
@@ -89,6 +99,11 @@ exports.postSignup = (req, res, next) => {
       path: "/signup",
       pageTitle: "Signup",
       errorMessage: errors.array()[0].msg,
+      oldInput: {
+        email: email,
+        password: password,
+        confirmPassword: confirmPassword,
+      },
     });
   }
   bcrypt
