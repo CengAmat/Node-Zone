12db26daff00ce2ce74ccedc0d5d93923c3101d7
cengamat <azurnaci@cultwinesltd.com>
2023-05-29 11:27:34 +0100

    Sending & Handling Background Requests

--- controllers/admin.js
+++ controllers/admin.js
@@ -162,8 +162,8 @@ exports.postEditProduct = (req, res, next) => {
     });
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const productId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+  const productId = req.params.productId;
   Product.findById(productId)
     .then((product) => {
       if (!product) {
@@ -174,12 +174,10 @@ exports.postDeleteProduct = (req, res, next) => {
     })
     .then((result) => {
       console.log("DELETED PRODUCT!");
-      res.redirect("/admin/products");
+      res.status(200).json({ message: "Success!" });
     })
     .catch((err) => {
-      const error = new Error(err);
-      error.httpStatusCode = 500;
-      return next(error);
+      res.status(500).json({ message: "Deleting product failed!" });
     });
 };
 
