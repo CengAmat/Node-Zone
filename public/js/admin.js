1e9fc83b5d535d920d4f58e3e7e93bfcbf503518
cengamat <azurnaci@cultwinesltd.com>
2023-05-29 11:34:15 +0100

    Manipulating the DOM

--- public/js/admin.js
+++ public/js/admin.js
@@ -2,12 +2,20 @@ const deleteProduct = (btn) => {
   const productId = btn.parentNode.querySelector("[name=productId]").value;
   const csrf = btn.parentNode.querySelector("[name=_csrf]").value;
 
+  const productElement = btn.closest("article");
+  console.log(productElement);
+
   fetch("/admin/product/" + productId, {
     method: "DELETE",
     headers: {
       "csrf-token": csrf,
     },
   })
-    .then((result) => console.log(result))
+    .then((result) => {
+      return result.json();
+    })
+    .then(() => {
+      productElement.parentNode.removeChild(productElement);
+    })
     .catch((err) => console.log(err));
 };
